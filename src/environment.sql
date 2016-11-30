f3a4908974afba645bb1e4ae082d9960a2546492
zhengwen <mail.zhengwen@gmail.com>
2016-11-30 11:47:31 +0800

    增加CPU监控

--- src/environment.sql
+++ src/environment.sql
@@ -38,6 +38,19 @@ GROUP BY DB_NAME(database_id) ,
         database_id
 ORDER BY 'Cached Size (MB)' DESC
 
+--查询CPU最高的10条SQL
+SELECT TOP 10 TEXT AS 'SQL Statement'
+    ,last_execution_time AS 'Last Execution Time'
+    ,(total_logical_reads + total_physical_reads + total_logical_writes) / execution_count AS [Average IO]
+    ,(total_worker_time / execution_count) / 1000000.0 AS [Average CPU Time (sec)]
+    ,(total_elapsed_time / execution_count) / 1000000.0 AS [Average Elapsed Time (sec)]
+    ,execution_count AS "Execution Count",qs.total_physical_reads,qs.total_logical_writes
+    ,qp.query_plan AS "Query Plan"
+FROM sys.dm_exec_query_stats qs
+CROSS APPLY sys.dm_exec_sql_text(qs.plan_handle) st
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
+ORDER BY total_elapsed_time / execution_count DESC
+
 --查询当前数据库的配置信息
 Select configuration_id ConfigurationId,
 name Name,
