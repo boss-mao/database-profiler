c428e71fd4441c7dcd2a30fa394d5bf2f8a481b9
zhengwen <mail.zhengwen@gmail.com>
2016-11-30 13:09:37 +0800

    增加存储过程执行次数监控

--- src/sql-perform-count.sql
+++ src/sql-perform-count.sql
@@ -1,3 +1,16 @@
+--查询CPU最高的10条SQL
+SELECT TOP 10 TEXT AS 'SQL Statement'
+    ,last_execution_time AS 'Last Execution Time'
+    ,(total_logical_reads + total_physical_reads + total_logical_writes) / execution_count AS [Average IO]
+    ,(total_worker_time / execution_count) / 1000000.0 AS [Average CPU Time (sec)]
+    ,(total_elapsed_time / execution_count) / 1000000.0 AS [Average Elapsed Time (sec)]
+    ,execution_count AS "Execution Count",qs.total_physical_reads,qs.total_logical_writes
+    ,qp.query_plan AS "Query Plan"
+FROM sys.dm_exec_query_stats qs
+CROSS APPLY sys.dm_exec_sql_text(qs.plan_handle) st
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
+ORDER BY total_elapsed_time / execution_count DESC
+
 
 --找出执行频繁的语句的SQL语句
 with aa as (
@@ -37,3 +50,21 @@ from aa
 where [text] not  like '%sp_MSupd_%' and  [text] not like '%sp_MSins_%' and  [text] not like '%sp_MSdel_%' 
 group by text
 order by 2  desc
+
+
+-- 查找逻辑读取最高的查询(存储过程)
+SELECT TOP ( 25 )
+        P.name AS [SP Name] ,
+        Deps.total_logical_reads AS [TotalLogicalReads] ,
+        deps.total_logical_reads / deps.execution_count AS [AvgLogicalReads] ,
+        deps.execution_count ,
+        ISNULL(deps.execution_count / DATEDIFF(Second, deps.cached_time,
+                                               GETDATE()), 0) AS [Calls/Second] ,
+        deps.total_elapsed_time ,
+        deps.total_elapsed_time / deps.execution_count AS [avg_elapsed_time] ,
+        deps.cached_time
+FROM    sys.procedures AS p
+        INNER JOIN sys.dm_exec_procedure_stats AS deps ON p.[Object_id] = deps.[Object_id]
+WHERE   deps.Database_id = DB_ID()
+ORDER BY deps.total_logical_reads DESC;
+
