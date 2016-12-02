cf6f5782e42fb3ee67a431bcbaf8b2b1903ba6e9
zhengwen <mail.zhengwen@gmail.com>
2016-12-02 16:47:02 +0800

    增加表数据页监控命令

--- src/environment.sql
+++ src/environment.sql
@@ -1,3 +1,16 @@
+--查询当前数据库的配置信息
+Select configuration_id ConfigurationId,
+name Name,
+description Description,
+Cast(value as int) value,
+Cast(minimum as int) Minimum,
+Cast(maximum as int) Maximum,
+Cast(value_in_use as int) ValueInUse,
+is_dynamic IsDynamic,
+is_advanced IsAdvanced
+From sys.configurations
+Order By is_advanced, name
+
 --检查SQL SERVER 当前已创建的线程数
 select count(*) from sys.dm_os_workers
 
@@ -39,30 +52,10 @@ GROUP BY DB_NAME(database_id) ,
 ORDER BY 'Cached Size (MB)' DESC
 
 
-
---查询当前数据库的配置信息
-Select configuration_id ConfigurationId,
-name Name,
-description Description,
-Cast(value as int) value,
-Cast(minimum as int) Minimum,
-Cast(maximum as int) Maximum,
-Cast(value_in_use as int) ValueInUse,
-is_dynamic IsDynamic,
-is_advanced IsAdvanced
-From sys.configurations
-Order By is_advanced, name
-
 --SQL SERVER  统计IO活动信息
 SET STATISTICS IO ON
 select top 10* from Table
 SET STATISTICS IO OFF
-
---查看当前进程的信息
-DBCC INPUTBUFFER(51)
-
---查看当前数据是否启用了快照隔离
-DBCC USEROPTIONS;
 
 --SQL SERVER 清除缓存SQL语句
 CHECKPOINT;
@@ -70,3 +63,20 @@ GO
 DBCC  FREEPROCCACHE      ---清空执行计划缓存
 DBCC DROPCLEANBUFFERS;   --清空数据缓存
 GO
+
+--查看当前进程的信息
+DBCC INPUTBUFFER(51)
+
+--查看当前数据是否启用了快照隔离
+DBCC USEROPTIONS;
+
+--查看摸个数据库数据表中的数据页类型
+--In_Row_Data: 分别为存储行内数据的
+	--LOB_Data: 存储Lob对象,Lob对象用于存储存在数据库的二进制文件
+	          --当这个类型的列出现时,原有的列会存储一个24字节的指针，而将具体的二进制数据存在LOB页中
+	--Row_Overflow_data:存储溢出数据的,使用Varchar,nvarchar等数据类型时，当行的大小不超过8060字节时，全部存在行内In-row data
+					--当varchar中存储的数据过多使得整行超过8060字节时，会将额外的部分存于Row-overflow data页中，
+					--如果update这列使得行大小减少到小于8060字节，则这行又会全部回到in-row data页
+					--text,ntext和image类型来说，每一列只要不为null,即使占用很小的数据，也需要额外分配一个LOB页
+DBCC IND ( Lawyer, [dbo.tb_Contract], -1)
+
