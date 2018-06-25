9962458a08e6dbdbe3c376626ad1b6bb035c862d
zhengwen <mail.zhengwen@gmail.com>
2018-06-25 09:34:54 +0800

    Update environment.sql

--- MySQL/environment.sql
+++ MySQL/environment.sql
@@ -1,3 +1,17 @@
+--查看连接信息以及连接执行的命令
+SHOW  PROCESSLIST
+
+--查看当前被锁住的表
+show OPEN TABLES where In_use > 0;
+
+--开启会话级别的profile
+SET profiling=1
+--查看所有的数据库操作执行过程
+SHOW PROFILES
+--查询单条语句profile
+SHOW PROFILE FOR QUERY 1
+
+
 --查看整个数据库服务的线程数
 show global status like 'Thread%';
 --刷新会话级别的计数器
@@ -7,15 +21,4 @@ SHOW STATUS
 
 --查看所表参数信息
 SHOW  TABLE STATUS
---查看连接信息以及连接执行的命令
-SHOW  PROCESSLIST
 
---开启会话级别的profile
-SET profiling=1
---查看所有的数据库操作执行过程
-SHOW PROFILES
---查询单条语句profile
-SHOW PROFILE FOR QUERY 1
-
---查看当前被锁住的表
-show OPEN TABLES where In_use > 0;
